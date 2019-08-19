061343545079b51a05a21a63f9e2c966c049c50b
Leon Wright <lwright@vertisjm.com>
2019-08-19 17:23:04 -0500

    First commit

---
+++ src/components/default-layout/default-layout.js
@@ -0,0 +1,21 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import "./default-layout.css"
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+
+Layout.propTypes = {}
+
+export default Layout
---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+import React from "react"
+import DefaultLayout from "./../components/default-layout/default-layout"
+
+const HomePage = () => <DefaultLayout></DefaultLayout>
+
+export default HomePage
