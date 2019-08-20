8b437c8f04a0c19b370ddc6f531ba03279b89b7d
Leon Wright <lwright@vertisjm.com>
2019-08-19 22:36:22 -0500

    Removed sass files and added typography.

--- src/components/default-layout/default-layout.js
+++ src/components/default-layout/default-layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import styled from "styled-components"
 import { device } from "./../../devices"
 import "./../../normalize.css"
+import "./../../typography.css"
 
 import menuBtn from "./../../images/menu-btn.svg"
 import profilePhto from "./../../images/leonwright.png"
