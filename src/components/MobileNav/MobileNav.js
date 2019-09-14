a6784a7e8c714645b305d457fc0fd5f53b4e0041
Leon Wright <lwright@vertisjm.com>
2019-09-14 12:45:50 -0500

    Finished nav menu

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
 
 const MobileNavMenu = styled.div`
@@ -7,22 +8,55 @@ const MobileNavMenu = styled.div`
   width: 100%;
   z-index: 1000;
   background-color: #fff;
-  display: flex;
+  display: ${props => (props.isOpen ? "block" : "none")};
 `
 
 const MobileNavCloseBtn = styled.img`
-  align-self: end;
-  margin-left: auto;
   padding: 2rem;
 `
 
-const MobileNav = () => {
+const NavItems = styled.div`
+  text-align: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const NavItem = styled(Link)`
+  text-decoration: none;
+  display: block;
+  font-size: 3.5rem;
+  color: var(--primary-color);
+
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`
+
+const MobileNav = ({ state, toggle }) => {
   return (
-    <MobileNavMenu>
-      <MobileNavCloseBtn
-        src={require("./../../images/close-btn.svg")}
-        alt="close-btn"
-      />
+    <MobileNavMenu isOpen={state.isOpen}>
+      <div style={{ textAlign: "end" }}>
+        <MobileNavCloseBtn
+          src={require("./../../images/close-btn.svg")}
+          alt="close-btn"
+          onClick={toggle}
+        />
+      </div>
+
+      <NavItems>
+        <NavItem to="/">Home</NavItem>
+        <NavItem
+          as="a"
+          href="https://docs.google.com/document/d/e/2PACX-1vTeiMzaQi9Pm9BSqayiySFBxJAlYCdZVF6uP_lXZTpF5ENLRogdfBxSvgRFmbaTCrdL_PB0c3ljpjwa/pub"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          My Resume
+        </NavItem>
+        <NavItem to="/contact">Contact</NavItem>
+      </NavItems>
     </MobileNavMenu>
   )
 }
